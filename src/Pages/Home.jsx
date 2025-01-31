3a0c3343670faf40bf89ee6580af0608e3881502
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-01-31 11:44:29 -0300

    add home page displaying all posts by friends

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,63 @@
+import React, { useContext, useEffect } from 'react';
+import UserContext from '../Context/userContext';
+import { useNavigate } from 'react-router-dom';
+import ErrorContext from '../Context/errorContext';
+import { useQuery } from '@tanstack/react-query';
+
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
+export default function Home() {
+	const { currentUser } = useContext(UserContext);
+	const { setError } = useContext(ErrorContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (currentUser === null) {
+			setError('Please login first.');
+			navigate('/login');
+		}
+	}, [currentUser, setError, navigate]);
+
+	const generateFeed = async () => {
+		const response = await fetch(
+			`${serverUrl}/api/posts/feed/${currentUser.user.id}`,
+			{
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' },
+			}
+		);
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(errorData.error || 'Error fetching posts');
+		}
+		return response.json();
+	};
+
+	const { data, error, isLoading } = useQuery({
+		queryKey: ['posts'],
+		queryFn: generateFeed,
+	});
+
+	useEffect(() => {
+		console.log('data', data);
+	}, [data]);
+
+	if (isLoading) return <p>Loading...</p>;
+	if (error) {
+		setError(error.message);
+		return <p>Error: {error.message}</p>;
+	}
+
+	return (
+		<div>
+			{data.map((post) => (
+				<ul key={post.id}>
+					<li>Author: {post.author.username}</li>
+					<li>{post.content}</li>
+					<li>Posted on: {post.createdAt}</li>
+					<button>Like</button>
+				</ul>
+			))}
+		</div>
+	);
+}
