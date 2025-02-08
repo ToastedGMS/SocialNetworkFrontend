db3c26cebb0a48da29cd92c7a9d25e7608aa4423
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-02-08 10:18:36 -0300

    add hook for creting new posts via frontend

---
+++ src/Hooks/useCreatePost.jsx
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+const serverUrl = import.meta.env.VITE_SERVER_URL;
+
+const createPost = async ({ content, user }) => {
+	const response = await fetch(`${serverUrl}/api/posts/new`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${user.token}`,
+		},
+		body: JSON.stringify({ authorID: user.user.id, content: content }),
+	});
+	if (!response.ok) {
+		const res = await response.json();
+		throw new Error(res.message || `Failed to create the post`);
+	}
+
+	const res = await response.json();
+	return res;
+};
+
+export function useCreatePost() {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: createPost,
+		onSuccess: () => {
+			queryClient.invalidateQueries(['posts']);
+		},
+		onError: (error) => {
+			console.error('Error creating post:', error.message);
+		},
+	});
+}
