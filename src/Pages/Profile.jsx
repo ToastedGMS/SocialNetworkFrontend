f6dd57081b10656d5146eff023ca15dfaf78db54
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-02-10 10:49:30 -0300

    display all user's posts on their profile page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import ErrorContext from '../Context/errorContext';
 import ProfileContext from '../Context/profileContext';
 import UserContext from '../Context/userContext';
+import PostContext from '../Context/postContext';
+import { useQuery } from '@tanstack/react-query';
+import Post from '../Reusable/Post';
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
@@ -10,6 +13,7 @@ export default function Profile() {
 	const { setError } = useContext(ErrorContext);
 	const { currentUser } = useContext(UserContext);
 	const { profile } = useContext(ProfileContext);
+	const { setPostVal } = useContext(PostContext);
 
 	const navigate = useNavigate();
 
@@ -20,15 +24,62 @@ export default function Profile() {
 		}
 	}, [currentUser, navigate, setError]);
 
+	async function getUserPosts() {
+		const response = await fetch(
+			`${serverUrl}/api/posts/read/?authorID=${profile.id}`,
+			{
+				method: 'GET',
+				headers: { 'Content-Type': 'application/json' },
+			}
+		);
+
+		if (!response.ok) {
+			const errorResponse = await response.json();
+			throw new Error(errorResponse.error || 'Error fetching user posts');
+		}
+
+		const res = await response.json();
+		return res;
+	}
+
+	const { data, error, isLoading } = useQuery({
+		queryKey: profile ? ['posts', profile.username] : ['posts', 'undefined'],
+		queryFn: getUserPosts,
+		enabled: !!profile,
+	});
+
 	return (
 		<>
+			{profile ? (
+				<div style={{ border: '1px solid black' }}>
+					<img
+						src={profile.profilePic}
+						alt={`${profile.username}'s profile picture`}
+					/>
+					<p>{profile.username}</p>
+					<p>{profile.bio}</p>{' '}
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
 			<div>
-				<img
-					src={profile.profilePic}
-					alt={`${profile.username}'s profile picture`}
-				/>
-				<p>{profile.username}</p>
-				<p>{profile.bio}</p>
+				<p>{profile.username}'s posts</p>
+				{isLoading && <p>Loading...</p>}
+				{error && <p>No posts found...</p>}
+				<div>
+					{data && data.length > 0 ? (
+						data.map((post) => (
+							<Post
+								data={post}
+								setPostVal={setPostVal}
+								currentUser={currentUser}
+								key={post.id}
+							/>
+						))
+					) : (
+						<p>No posts found...</p>
+					)}
+				</div>
 			</div>
 		</>
 	);
