99766197671c449449ae21d77a85737ab4266e86
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-02-28 13:38:35 -0300

    add profile update styling

--- src/Pages/UpdateProfile.jsx
+++ src/Pages/UpdateProfile.jsx
@@ -4,7 +4,7 @@ import ErrorContext from '../Context/errorContext';
 import UserContext from '../Context/userContext';
 import UpdateProfileBtn from '../Reusable/UpdateProfileBtn';
 import ErrorMessage from '../Reusable/ErrorMessage';
-
+import style from './styles/UpdateProfile.module.css';
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export default function UpdateProfile() {
@@ -67,61 +67,59 @@ export default function UpdateProfile() {
 
 	return (
 		<>
-			<div style={{ border: '1px solid black' }}>
+			<div className={style.currentProfile}>
 				<img src={profilePicVal} alt={`${usernameVal}'s profile picture`} />
 				<p>{usernameVal}</p>
 				<p>{bioVal}</p>
 			</div>
 
-			<form onSubmit={(e) => e.preventDefault()}>
-				<label htmlFor="username">Username: </label>
-				<input
-					type="text"
-					name="username"
-					id="username"
-					value={usernameVal}
-					onChange={(e) => setUsernameVal(e.target.value)}
-				/>
-
-				<label htmlFor="bio">Bio: </label>
-				<input
-					type="text"
-					name="bio"
-					id="bio"
-					value={bioVal}
-					onChange={(e) => setBioVal(e.target.value)}
-				/>
-
-				<label htmlFor="profilePic">Profile Picture: </label>
-				<input type="file" onChange={handleFileChange} />
-				<button onClick={handleUpload}>Upload</button>
-
-				{uploadResponse && (
-					<div>
-						<p>{uploadResponse.message}</p>
-						<p>Image preview:</p>
-						<img
-							src={uploadResponse.fileUrl}
-							alt="Uploaded preview"
-							width={'200px'}
-						/>
-					</div>
-				)}
-
-				{isValidPic && isFormValid && (
-					<UpdateProfileBtn
-						currentUser={currentUser}
-						content={{
-							username: usernameVal,
-							email: currentUser.user.email,
-							bio: bioVal,
-							profilePic: profilePicVal,
-						}}
+			<div className={style.container}>
+				<form className={style.form} onSubmit={(e) => e.preventDefault()}>
+					<label htmlFor="username">Username: </label>
+					<input
+						type="text"
+						name="username"
+						id="username"
+						value={usernameVal}
+						onChange={(e) => setUsernameVal(e.target.value)}
 					/>
-				)}
 
-				{!isFormValid && <ErrorMessage error={'Username cannot be empty!'} />}
-			</form>
+					<label htmlFor="bio">Bio: </label>
+					<input
+						type="text"
+						name="bio"
+						id="bio"
+						value={bioVal}
+						onChange={(e) => setBioVal(e.target.value)}
+					/>
+
+					<label htmlFor="profilePic">Profile Picture: </label>
+					<input type="file" onChange={handleFileChange} />
+					<button onClick={handleUpload}>Upload</button>
+
+					{uploadResponse && (
+						<div>
+							<p>{uploadResponse.message}</p>
+							<p>Image preview:</p>
+							<img src={uploadResponse.fileUrl} alt="Uploaded preview" />
+						</div>
+					)}
+
+					{isValidPic && isFormValid && (
+						<UpdateProfileBtn
+							currentUser={currentUser}
+							content={{
+								username: usernameVal,
+								email: currentUser.user.email,
+								bio: bioVal,
+								profilePic: profilePicVal,
+							}}
+						/>
+					)}
+
+					{!isFormValid && <ErrorMessage error={'Username cannot be empty!'} />}
+				</form>
+			</div>
 		</>
 	);
 }
