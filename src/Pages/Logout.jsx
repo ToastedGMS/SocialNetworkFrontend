e9dc1899a242c806e6feb70687dd20c957b2a898
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-03-01 13:09:39 -0300

    fix user actions still showing on guest logout

--- src/Pages/Logout.jsx
+++ src/Pages/Logout.jsx
@@ -1,13 +1,16 @@
 import { useContext, useEffect } from 'react';
 import UserContext from '../Context/userContext';
 import { useNavigate } from 'react-router-dom';
+import GuestContext from '../Context/guestContext';
 
 export default function Logout() {
 	const { currentUser, setCurrentUser } = useContext(UserContext);
+	const { setIsGuest } = useContext(GuestContext);
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		setCurrentUser(null);
+		setIsGuest(false);
 		navigate('/login');
 	}, [currentUser, setCurrentUser]);
 
